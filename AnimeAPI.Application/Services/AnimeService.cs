8244f7658e10325245e0cd71366caa00280f782a
matheus55391 <matheus.manulo55@gmail.com>
2025-02-18 22:56:13 -0300

    feat/implementado endpoints de atualizar e deletar animes

--- AnimeAPI.Application/Services/AnimeService.cs
+++ AnimeAPI.Application/Services/AnimeService.cs
@@ -18,14 +18,38 @@ namespace AnimeAPI.Application.Services
             return await _animeRepository.GetAllAsync();
         }
 
-        public async Task<Anime> CadastrarAnimeAsync(CreateAnimeRequestDto anime)
+        public async Task<Anime> CreateAsync(CreateAnimeRequestDto anime)
         {
-
-
             var newAnime = new Anime(anime.Name, anime.Director, anime.Summary);
 
             await _animeRepository.AddAsync(newAnime);
             return newAnime;
+        }
+
+
+        public async Task<Anime> UpdateAsync(UpdateAnimeRequestDto anime)
+        {
+            var updatedAnime = new Anime(
+                anime.Id,
+                anime.Name,
+                anime.Director,
+                anime.Summary
+            );
+            await _animeRepository.UpdateAsync(updatedAnime);
+
+            return updatedAnime;
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var anime = await _animeRepository.GetByIdAsync(id);
+
+            if (anime == null)
+            {
+                throw new KeyNotFoundException("Anime n o encontrado");
+            }
+
+            await _animeRepository.DeleteAsync(id);
         }
     }
 }
